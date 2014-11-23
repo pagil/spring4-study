fe1f96167766c4fd4cf6f60a04fcba56c0310eaa
truemate <truemate@test.com>
2014-11-23 22:20:12 +1100

    Changes for: Fix: Use @RestController instead of @Controller to avoid exception: javax.servlet.ServletException: Circular view path []: would dispatch to the current handler URL [/] again. Check your View...
    learned from the following video
    
    http://saurzcode.wordpress.com/2014/08/19/how-to-configure-swagger-to-generate-restful-api-doc-for-your-spring-boot-web-application/

--- springboot02/src/main/java/com/example/controller/ContactsController.java
+++ springboot02/src/main/java/com/example/controller/ContactsController.java
@@ -5,10 +5,13 @@ import com.example.service.ContactsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+// Use @RestController instead of @Controller to avoid exception:
+// javax.servlet.ServletException: Circular view path []: would dispatch to the current handler URL [/] again. Check your View...
+@RestController
 public class ContactsController {
 
     @Autowired
