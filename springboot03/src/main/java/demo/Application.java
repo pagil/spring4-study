ea2503f36f19ebb86d2ff2ab7917e07e75ba09a5
truemate <truemate@test.com>
2014-11-24 23:39:19 +1100

    Implementation of the changes described in this lesson: https://spring.io/blog/2014/11/20/screencast-how-to-create-a-restful-app-in-five-minutes-or-less
    - JPA
    - Hypersonic DB
    - Spring Boot
    - Spring Repositories
    
    The result can be tested using URL:
    http://localhost:8080/employees/1

--- springboot03/src/main/java/demo/Application.java
+++ springboot03/src/main/java/demo/Application.java
@@ -1,16 +1,31 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    private void init() {
+        Employee employee = new Employee();
+        employee.setFirstName("Bilbo");
+        employee.setLastName("Baggins");
+        employee.setTitle("thief");
+        employeeRepository.save(employee);
     }
 }
