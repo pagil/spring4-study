70fb7bd8e1c9d9f611ba65950c57b5041e65e334
truemate <truemate@test.com>
2014-12-14 13:09:49 +1100

    Clean up the code.

--- springboot05/src/main/java/demo/controller/HeadersController.java
+++ springboot05/src/main/java/demo/controller/HeadersController.java
@@ -1,7 +1,5 @@
 package demo.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
